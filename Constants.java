d19dfd18d481719797bec7889a155f6dea4a36ae
Michael Wineburg <mikedoubleyouu@Michael-Wineburgs-MacBook-Pro.local>
2011-07-07 21:03:42 -0400

    sql

---
+++ Constants.java
@@ -0,0 +1,22 @@
+/***
+ * Excerpted from "Hello, Android!",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/eband for more book information.
+***/
+package org.example.events;
+
+import android.provider.BaseColumns;
+
+public interface Constants extends BaseColumns {
+   public static final String TABLE_NAME = "mikesevents";
+
+   // Columns in the Events database
+   public static final String TIME = "time";
+   public static final String TITLE = "title";
+   public static final String USERNAME="username";
+   public static final String HIGHSCORE="highscore";
+   public static final String DATE="date";
+}
